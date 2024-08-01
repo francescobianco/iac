7aeb0eab163381e98832be525ca2d6e35bc10862
francesco <info.francescobianco@gmail.com>
2024-08-01 12:46:11 +0200

    Testing

--- .deploy.sh
+++ .deploy.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 set -e
 
-IAC_RESOURCE=$1
-IAC_VARIABLES=$(grep "^host=[a-z0-9.]* iac=$1" "${HOME}/.hosts" | head -1)
+IAC_RESOURCE=$(echo "$1" | sed 's/\/*$//g')
+IAC_VARIABLES=$(grep "^host=[a-z0-9.]* name=.* iac=${IAC_RESOURCE}" "${HOME}/.hosts" | head -1)
 
 if [ -z "${IAC_VARIABLES}" ]; then
   echo "Resource host not found: $1"
   exit 1
 fi
 
-echo "Hello World! $IAC_RESOURCE"
+for variable in ${IAC_VARIABLES}; do
+  declare "$variable"
+  [ -n "${host}" ] && SSH_HOST="${host}"
+  [ -n "${user}" ] && SSH_USER="${user}"
+  [ -n "${password}" ] && SSH_PASSWORD="${password}"
+done
+
+#echo "Hello World! $IAC_RESOURCE $IAC_VARIABLES"
+
+sshpass -p "${SSH_PASSWORD}" ssh "${SSH_USER}@${SSH_HOST}" -p "${SSH_PORT:-22}" bash -s -- "$IAC_VARIABLES" << 'EOF'
+  apt-get update >/dev/null 2>&1
+  command -v git >/dev/null 2>&1 || apt-get install -y git
+  test -d /opt/iac || git config --global --add safe.directory /opt/iac
+  test -d /opt/iac || git clone https://github.com/francescobianco/iac /opt/iac
+  echo "Hello World!"
+EOF
+
+sshpass -p "${SSH_PASSWORD}" ssh "${SSH_USER}@${SSH_HOST}" -p "${SSH_PORT:-22}" bash -s -- "$IAC_VARIABLES"
