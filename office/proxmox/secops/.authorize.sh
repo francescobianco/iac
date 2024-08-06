2034e983fa06536aa61817dc4a5320dfc25e722a
francesco <info.francescobianco@gmail.com>
2024-08-06 14:38:49 +0200

    Deploy resource from 'office/proxmox/secops/' directory

--- office/proxmox/secops/.authorize.sh
+++ office/proxmox/secops/.authorize.sh
@@ -9,4 +9,6 @@ for variable in "$@"; do
 done
 
 sshpass -p "${SSH_PASSWORD}" scp -P "${SSH_PORT:-22}" "${HOME}/.hosts" "${SSH_USER}@${SSH_HOST}:/root/.hosts"
+sshpass -p "${SSH_PASSWORD}" scp -P "${SSH_PORT:-22}" "${HOME}/.ssh/id_rsa" "${SSH_USER}@${SSH_HOST}:/root/.ssh/id_rsa"
+sshpass -p "${SSH_PASSWORD}" scp -P "${SSH_PORT:-22}" "${HOME}/.ssh/id_rsa.pub" "${SSH_USER}@${SSH_HOST}:/root/.ssh/id_rsa.pub"
 sshpass -p "${SSH_PASSWORD}" scp -P "${SSH_PORT:-22}" "${HOME}/.config/rclone/rclone.conf" "${SSH_USER}@${SSH_HOST}:/root/.config/rclone/rclone.conf"
